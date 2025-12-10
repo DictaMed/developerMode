f40072edd2d152de515ba2e83f9d9d0d09cedf26
DictaMed <akio963@gmail.com>
2025-12-10 19:13:45 +0100

    firebase

--- firebase-config.js
+++ firebase-config.js
@@ -1,17 +1,21 @@
-// Firebase Configuration for DictaMed
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
 // Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyB7drb3A3xL_JVZz_tLGtsRCc4ZZlFfSNQ",
-    authDomain: "dictamed-2025.firebaseapp.com",
-    projectId: "dictamed-2025",
-    storageBucket: "dictamed-2025.firebasestorage.app",
-    messagingSenderId: "565675197934",
-    appId: "1:565675197934:web:d3bd5a9f86d274e581baff",
-    measurementId: "G-R3MN277B7F"
+  apiKey: "AIzaSyC9XYvHxbp3VW0PCW0W7xfoWuiMxsjoUZE",
+  authDomain: "dictamed2025.firebaseapp.com",
+  projectId: "dictamed2025",
+  storageBucket: "dictamed2025.firebasestorage.app",
+  messagingSenderId: "242034923776",
+  appId: "1:242034923776:web:bd315e890c715b1d263be5",
+  measurementId: "G-1B8DZ4B73R"
 };
 
 // Initialize Firebase
-firebase.initializeApp(firebaseConfig);
-
-// Make Firebase available globally
-window.firebase = firebase;
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
